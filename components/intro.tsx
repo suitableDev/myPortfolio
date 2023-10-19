abbaf85f0c871e4238f6ae04f472fcea23c24b59
scott <rinzencedev@gmail.com>
2023-10-19 17:58:20 +0100

    created intro component with temporary content that will need updating later

---
+++ components/intro.tsx
@@ -0,0 +1,102 @@
+"use client"
+import Image from "next/image"
+import React from "react"
+import { motion } from "framer-motion"
+import Link from "next/link"
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+        <div className="flex items-center justify-center">
+            <div className="relative">
+                <motion.div
+                initial={{ opacity: 0, scale: 0}}
+                animate={{ opacity: 1, scale: 1}}
+                transition={{
+                    type: "tween",
+                    duration: 0.2,
+                }}
+                >
+                <Image 
+                src="/me.jpg" 
+                alt="Scott James front end developer"
+                width="192"
+                height="192"
+                quality="95"
+                priority={true}
+                className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+                />
+                </motion.div>
+                <motion.span 
+                className="absolute bottom-0 left-0 text-4xl"
+                initial={{ opacity: 0, scale: 0}}
+                animate={{ opacity: 1, scale: 1}}
+                transition={{
+                    type: "spring",
+                    stiffness: 125,
+                    delay: 0.1,
+                    duration: 0.2,
+                }}
+                >
+                👋
+                </motion.span>
+            </div>
+        </div>
+        <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Scott.</span> I'm a{" "}
+        <span className="font-bold">front-end developer</span> with a{" "}
+        <span className="font-bold">zesty</span> lust for life. I bloody love
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.h1>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack"
+          href="https://images.squarespace-cdn.com/content/v1/62a793833847c539b0967c69/1657795316505-ZUWEOL5OIC4TU6QZSGIZ/image-asset.jpeg?format=1000w"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack"
+          href="https://miro.medium.com/v2/resize:fit:640/format:webp/1*NVnMcC9xCjty1FOofU5L_g.png"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  )
+}
