11ed26543c80aed5a3cf0ede0433c35a8a20b455
scott <rinzencedev@gmail.com>
2023-11-14 16:27:22 +0000

    Header functionalilty complete, added skills and experience

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Intro from "@/components/intro"
 import SectionDivder from "@/components/section-divder"
 import About from "@/components/about"
 import Projects from "@/components/projects"
+import Skills from "@/components/skills"
+import Experience from "@/components/experience"
+import Contact from "@/components/contact"
 
 export default function Home() {
   return (
@@ -10,6 +13,9 @@ export default function Home() {
       <SectionDivder />
       <About />
       <Projects />
+      <Skills />
+      <Experience />
+      <Contact />
     </main>
   )
 }
---
+++ components/experience.tsx
@@ -0,0 +1,50 @@
+"use client"
+import React from "react"
+import SectionHeading from "./sectionHeading"
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+import { experiencesData } from "@/lib/data"
+import { useSectionInView } from "@/lib/hooks"
+
+export default function Experience() {
+  const { ref } = useSectionInView("Experience")
+  return (
+    <section
+    ref={ref} 
+    id="experience"
+    className="scroll-mt-28 mb-28 sm:mb-40"
+    >
+        <SectionHeading>My Experience</SectionHeading>
+        <VerticalTimeline lineColor="">
+            {
+            experiencesData.map((item, index) => (
+              <React.Fragment key={index}>
+               <VerticalTimelineElement
+               contentStyle={{
+                 background:"#f3f4f6",
+                 boxShadow: "none",
+                 border: "1px solid rgba(0, 0, 0, 0.05)",
+                 textAlign: "left",
+                 padding: "1.3rem 2rem",
+               }}
+               contentArrowStyle={{
+                 borderRight:"0.4rem solid #9ca3af",
+               }}
+              //  date={item.date}
+               icon={item.icon}
+               iconStyle={{
+                 background: "white" ,
+                 fontSize: "1.5rem",
+               }}
+             >
+                <h3>{item.title}</h3>
+                <p>{item.location}</p>
+                <p>{item.description}</p>
+              </VerticalTimelineElement>
+              </React.Fragment>
+            ))}
+        </VerticalTimeline>
+
+    </section>
+  )
+}
