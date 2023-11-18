b65d90961770dae4acb5f16565b4fcdd58a3f991
scott <rinzencedev@gmail.com>
2023-11-18 16:47:59 +0000

    pre-react-dom

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,7 +1,9 @@
 "use server"
 
+import React from "react"
 import { Resend } from "resend"
 import { validateString, getErrorMessage } from "@/lib/utils"
+import ContactFormEmail from "@/email/contact-form-email"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
@@ -28,6 +30,10 @@ export const sendEmail = async (formData: FormData) => {
         subject: "Message from contact form",
         reply_to: senderEmail,
         text: message,
+        // react: React.createElement(ContactFormEmail, {
+        //   message: message,
+        //   senderEmail: senderEmail,
+        // })
         })
   } catch(error: unknown){
     return {
